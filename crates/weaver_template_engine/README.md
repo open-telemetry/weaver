cadc052ab9bb8c60192380626e5893fcc56cad1e
Laurent Querel <l.querel@f5.com>
2024-02-17 17:46:15 -0800

    feat(template): replace tera with minijinja to improve error handling

---
+++ crates/weaver_template_engine/README.md
@@ -0,0 +1,6 @@
+# Template Engine
+
+Status: **Work-In-Progress**
+
+This crate extends the `MiniJinja` template engine with custom filters and
+functions.
---
+++ crates/weaver_template_engine/src/lib.rs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//! This crate extends the MiniJinja template engine to add helper functions
+//! and filters for working with semantic convention registries and telemetry
+//! schemas.
+
+#![deny(
+missing_docs,
+clippy::print_stdout,
+unstable_features,
+unused_import_braces,
+unused_qualifications,
+unused_results,
+unused_extern_crates
+)]
