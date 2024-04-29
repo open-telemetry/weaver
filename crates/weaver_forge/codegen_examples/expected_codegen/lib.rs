37acc4b68523fadcf654abf3a7be1cd70205a716
Laurent Querel <l.querel@f5.com>
2024-04-29 14:32:55 -0700

    chore(forge): Simplify mini_registry

--- crates/weaver_forge/codegen_examples/expected_codegen/lib.rs
+++ crates/weaver_forge/codegen_examples/expected_codegen/lib.rs
@@ -11,22 +11,10 @@ use opentelemetry::{Key, KeyValue, StringValue};
 
 /// These attributes may be used to describe the client in a connection-based network interaction where there is one side that initiates the connection (the client is the side that initiates the connection). This covers all TCP network interactions since TCP is connection-based and one side initiates the connection (an exception is made for peer-to-peer communication over TCP where the "user-facing" surface of the protocol / API doesn't expose a clear notion of client and server). This also covers UDP network interactions where one side initiates the interaction, e.g. QUIC (HTTP/3) and DNS.
 pub mod client;
-/// These attributes may be used for any disk related operation.
-pub mod disk;
-/// This document defines the shared attributes used to report an error.
-pub mod error;
 /// This document defines the shared attributes used to report a single exception associated with a span or log.
 pub mod exception;
 /// This document defines semantic convention attributes in the HTTP namespace.
 pub mod http;
-/// These attributes may be used for any network related operation.
-pub mod network;
-/// The operating system (OS) on which the process represented by this resource is running.
-pub mod os;
-/// Attributes reserved for OpenTelemetry
-pub mod otel;
-/// Attributes used by non-OTLP exporters to represent OpenTelemetry Scope's concepts.
-pub mod otel_scope;
 
 /// A typed attribute key.
 pub struct AttributeKey<T> {
