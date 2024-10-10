2f40d702544911eab7a186024d08b14cb2780956
Josh Suereth <joshuasuereth@google.com>
2024-10-10 18:55:14 -0400

    Fix more tests.

--- crates/weaver_resolver/src/registry.rs
+++ crates/weaver_resolver/src/registry.rs
@@ -811,22 +811,24 @@ mod tests {
             println!("Testing `{}`", test_dir);
 
             let registry_id = "default";
-            let result = 
-                SemConvRegistry::try_from_path_pattern(
-                    registry_id,
-                    &format!("{}/registry/*.yaml", test_dir),
-                );
-            let sc_specs =
-                result
+            let result = SemConvRegistry::try_from_path_pattern(
+                registry_id,
+                &format!("{}/registry/*.yaml", test_dir),
+            );
+            let sc_specs = result
                 .ignore(|e| {
                     // Ignore prefix errors on tests of prefix.
-                    test_dir.contains("prefix") && matches!(e, weaver_semconv::Error::InvalidGroupUsesPrefix { 
-                        path_or_url: _, group_id: _
-                    })
+                    test_dir.contains("prefix")
+                        && matches!(
+                            e,
+                            weaver_semconv::Error::InvalidGroupUsesPrefix {
+                                path_or_url: _,
+                                group_id: _
+                            }
+                        )
                 })
                 .into_result_failing_non_fatal()
                 .expect("Failed to load semconv specs");
-            
 
             let mut attr_catalog = AttributeCatalog::default();
             let observed_registry =
@@ -872,9 +874,14 @@ mod tests {
             .expect("Failed to deserialize expected registry");
 
             assert_eq!(
-                observed_registry, expected_registry,
+                observed_registry,
+                expected_registry,
                 "Expected and observed registry don't match for `{}`.\nDiff from expected:\n{}",
-                test_dir, weaver_diff::diff_output(&to_json(&expected_registry), &to_json(&observed_registry))
+                test_dir,
+                weaver_diff::diff_output(
+                    &to_json(&expected_registry),
+                    &to_json(&observed_registry)
+                )
             );
 
             // let yaml = serde_yaml::to_string(&observed_registry).unwrap();
