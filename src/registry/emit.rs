881f10ccc9032a82990b6b784bbcdab2839401e4
jerbly <jeremyblythe@gmail.com>
2025-01-12 14:52:51 -0500

    template types supported

--- src/registry/emit.rs
+++ src/registry/emit.rs
@@ -9,7 +9,9 @@ use opentelemetry_sdk::Resource;
 use weaver_common::diagnostic::DiagnosticMessages;
 use weaver_common::Logger;
 use weaver_resolved_schema::attribute::Attribute;
-use weaver_semconv::attribute::{AttributeType, PrimitiveOrArrayTypeSpec};
+use weaver_semconv::attribute::{
+    AttributeType, Examples, PrimitiveOrArrayTypeSpec, TemplateTypeSpec,
+};
 use weaver_semconv::group::{GroupType, SpanKindSpec};
 
 use crate::registry::{PolicyArgs, RegistryArgs};
@@ -17,10 +19,9 @@ use crate::util::prepare_main_registry;
 use crate::{DiagnosticArgs, ExitDirectives};
 
 use opentelemetry::global;
-use opentelemetry::trace::{SpanKind, TraceError, Tracer};
+use opentelemetry::trace::{SpanKind, TraceContextExt, TraceError, Tracer};
 use opentelemetry::{Array, KeyValue, Value};
 use opentelemetry_sdk::trace as sdktrace;
-use opentelemetry_sdk::trace::{Span, TracerProvider};
 
 /// Parameters for the `registry emit` sub-command
 #[derive(Debug, Args)]
@@ -39,28 +40,95 @@ pub struct RegistryEmitArgs {
 }
 
 // For the given attribute, return a name/value pair.
-// The name is the name of the attribute.
-// The value is data compatible with the type of the attribute
-// TODO Get the data from the examples
+// Values are generated based on the attribute type and examples where possible.
 fn get_attribute_name_value(attribute: &Attribute) -> KeyValue {
     let name = attribute.name.clone();
-    let value = match &attribute.r#type {
-        AttributeType::PrimitiveOrArray(primitive_or_array) => match primitive_or_array {
-            PrimitiveOrArrayTypeSpec::Boolean => Value::Bool(true),
-            PrimitiveOrArrayTypeSpec::Int => Value::I64(42),
-            PrimitiveOrArrayTypeSpec::Double => Value::F64(3.13),
-            PrimitiveOrArrayTypeSpec::String => Value::String("value".into()),
-            PrimitiveOrArrayTypeSpec::Booleans => Value::Array(Array::Bool(vec![true, false])),
-            PrimitiveOrArrayTypeSpec::Ints => Value::Array(Array::I64(vec![42, 43])),
-            PrimitiveOrArrayTypeSpec::Doubles => Value::Array(Array::F64(vec![3.13, 3.15])),
-            PrimitiveOrArrayTypeSpec::Strings => {
-                Value::Array(Array::String(vec!["value1".into(), "value2".into()]))
-            }
-        },
-        AttributeType::Enum { members, .. } => Value::String(members[0].value.to_string().into()),
-        AttributeType::Template(_) => "value".into(),
-    };
-    KeyValue::new(name, value)
+    match &attribute.r#type {
+        AttributeType::PrimitiveOrArray(primitive_or_array) => {
+            let value = match primitive_or_array {
+                PrimitiveOrArrayTypeSpec::Boolean => Value::Bool(true),
+                PrimitiveOrArrayTypeSpec::Int => match &attribute.examples {
+                    Some(Examples::Int(i)) => Value::I64(*i),
+                    Some(Examples::Ints(ints)) => Value::I64(*ints.first().unwrap_or(&42)),
+                    _ => Value::I64(42),
+                },
+                PrimitiveOrArrayTypeSpec::Double => match &attribute.examples {
+                    Some(Examples::Double(d)) => Value::F64(f64::from(*d)),
+                    Some(Examples::Doubles(doubles)) => {
+                        Value::F64(f64::from(*doubles.first().unwrap_or((&3.13).into())))
+                    }
+                    _ => Value::F64(3.15),
+                },
+                PrimitiveOrArrayTypeSpec::String => match &attribute.examples {
+                    Some(Examples::String(s)) => Value::String(s.clone().into()),
+                    Some(Examples::Strings(strings)) => Value::String(
+                        strings
+                            .first()
+                            .unwrap_or(&"value".to_owned())
+                            .clone()
+                            .into(),
+                    ),
+                    _ => Value::String("value".into()),
+                },
+                PrimitiveOrArrayTypeSpec::Booleans => Value::Array(Array::Bool(vec![true, false])),
+                PrimitiveOrArrayTypeSpec::Ints => match &attribute.examples {
+                    Some(Examples::Ints(ints)) => Value::Array(Array::I64(ints.to_vec())),
+                    Some(Examples::ListOfInts(list_of_ints)) => Value::Array(Array::I64(
+                        list_of_ints.first().unwrap_or(&vec![42, 43]).to_vec(),
+                    )),
+                    _ => Value::Array(Array::I64(vec![42, 43])),
+                },
+                PrimitiveOrArrayTypeSpec::Doubles => match &attribute.examples {
+                    Some(Examples::Doubles(doubles)) => {
+                        Value::Array(Array::F64(doubles.iter().map(|d| f64::from(*d)).collect()))
+                    }
+                    Some(Examples::ListOfDoubles(list_of_doubles)) => Value::Array(Array::F64(
+                        list_of_doubles
+                            .first()
+                            .unwrap_or(&vec![(3.13).into(), (3.15).into()])
+                            .iter()
+                            .map(|d| f64::from(*d))
+                            .collect(),
+                    )),
+                    _ => Value::Array(Array::F64(vec![3.13, 3.15])),
+                },
+                PrimitiveOrArrayTypeSpec::Strings => match &attribute.examples {
+                    Some(Examples::Strings(strings)) => Value::Array(Array::String(
+                        strings.iter().map(|s| s.clone().into()).collect(),
+                    )),
+                    Some(Examples::ListOfStrings(list_of_strings)) => Value::Array(Array::String(
+                        list_of_strings
+                            .first()
+                            .unwrap_or(&vec!["value1".to_owned(), "value2".to_owned()])
+                            .iter()
+                            .map(|s| s.clone().into())
+                            .collect(),
+                    )),
+                    _ => Value::Array(Array::String(vec!["value1".into(), "value2".into()])),
+                },
+            };
+            KeyValue::new(name, value)
+        }
+        AttributeType::Enum { members, .. } => {
+            KeyValue::new(name, Value::String(members[0].value.to_string().into()))
+        }
+        AttributeType::Template(template_type_spec) => {
+            let value = match template_type_spec {
+                TemplateTypeSpec::String => Value::String("template_value".into()),
+                TemplateTypeSpec::Int => Value::I64(42),
+                TemplateTypeSpec::Double => Value::F64(3.13),
+                TemplateTypeSpec::Boolean => Value::Bool(true),
+                TemplateTypeSpec::Strings => Value::Array(Array::String(vec![
+                    "template_value1".into(),
+                    "template_value2".into(),
+                ])),
+                TemplateTypeSpec::Ints => Value::Array(Array::I64(vec![42, 43])),
+                TemplateTypeSpec::Doubles => Value::Array(Array::F64(vec![3.13, 3.15])),
+                TemplateTypeSpec::Booleans => Value::Array(Array::Bool(vec![true, false])),
+            };
+            KeyValue::new(format!("{name}.key"), value)
+        }
+    }
 }
 
 /// Convert the span kind to an OTLP span kind.
@@ -80,16 +148,14 @@ fn init_tracer_provider() -> Result<sdktrace::TracerProvider, TraceError> {
     let exporter = opentelemetry_otlp::SpanExporter::builder()
         .with_tonic()
         .with_endpoint("http://localhost:4317")
-        .with_timeout(std::time::Duration::from_secs(3))
         .build()?;
     Ok(sdktrace::TracerProvider::builder()
-        .with_resource(Resource::new(vec![KeyValue::new("service.name", "weaver")]))
+        .with_resource(Resource::new(vec![KeyValue::new("service.name", "weaver")])) // TODO meta semconv!
         .with_batch_exporter(exporter, opentelemetry_sdk::runtime::Tokio)
         .build())
 }
 
-/// Resolve a semantic convention registry and write the resolved schema to a
-/// file or print it to stdout.
+/// Emit all spans in the resolved registry to the OTLP receiver.
 pub(crate) fn command(
     logger: impl Logger + Sync + Clone,
     args: &RegistryEmitArgs,
@@ -107,22 +173,27 @@ pub(crate) fn command(
         let tracer_provider = init_tracer_provider().expect("OTLP Tracer Provider must be created");
         let _ = global::set_tracer_provider(tracer_provider.clone());
         let tracer = global::tracer("weaver");
-        // Go through the resolved registry and emit each group of type Span to the
-        // OTLP receiver.
-        for group in registry.groups.iter() {
-            if group.r#type == GroupType::Span {
-                logger.success(&format!("Emitting {}", group.id));
+        // Start a parent span here and use this context to create child spans
+        tracer.in_span("weaver.emit", |cx| {
+            let span = cx.span();
+            span.set_attribute(KeyValue::new(
+                "weaver.registry_path", // TODO meta semconv!
+                args.registry.registry.to_string(),
+            ));
 
-                let span = tracer
-                    .span_builder(group.id.clone())
-                    .with_kind(otel_span_kind(group.span_kind.as_ref()))
-                    .with_attributes(group.attributes.iter().map(get_attribute_name_value))
-                    .start(&tracer);
-                drop(span);
+            // Emit each span to the OTLP receiver.
+            for group in registry.groups.iter() {
+                if group.r#type == GroupType::Span {
+                    logger.success(&format!("Emitting {}", group.id));
+
+                    let _span = tracer
+                        .span_builder(group.id.clone())
+                        .with_kind(otel_span_kind(group.span_kind.as_ref()))
+                        .with_attributes(group.attributes.iter().map(get_attribute_name_value))
+                        .start_with_context(&tracer, &cx);
+                }
             }
-        }
-
-        // Shutdown trace pipeline
+        });
         global::shutdown_tracer_provider();
     });
 
@@ -148,7 +219,8 @@ mod tests {
     use crate::run_command;
 
     #[test]
-    fn test_registry_resolve() {
+    fn test_registry_emit() {
+        // TODO This could use the OTLP standard output exporter and check the output.
         let logger = TestLogger::new();
         let cli = Cli {
             debug: 1,
