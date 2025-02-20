789934c8bed77d9dd79923ab70b74292ea653826
Liudmila Molkova <limolkova@microsoft.com>
2025-02-20 10:12:08 -0800

    fmt

--- crates/weaver_semconv/src/registry.rs
+++ crates/weaver_semconv/src/registry.rs
@@ -244,7 +244,8 @@ impl SemConvRegistry {
                 .iter()
                 .flat_map(|sc| sc.spec.groups.iter().map(|g| g.r#type.clone()))
                 .fold(HashMap::new(), |mut acc, group_type| {
-                    *acc.entry(group_type.expect("group type is required")).or_insert(0) += 1;
+                    *acc.entry(group_type.expect("group type is required"))
+                        .or_insert(0) += 1;
                     acc
                 }),
             attribute_count: self.attributes.len(),
