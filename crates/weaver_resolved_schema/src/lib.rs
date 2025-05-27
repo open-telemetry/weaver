f60d687540788fae39b777aeef0f60de5d87768d
Liudmila Molkova <limolkova@microsoft.com>
2025-05-27 10:59:30 -0700

    Fail on unstructured deprecated property  under --future flag (#737)
    * Fail on unstructured deprecated property  under --future flag
    
    * format
    
    * undo dep update
    
    * up
    
    * add role
    
    * changelog

--- crates/weaver_resolved_schema/src/lib.rs
+++ crates/weaver_resolved_schema/src/lib.rs
@@ -329,7 +329,7 @@ impl ResolvedTelemetrySchema {
                                 },
                             );
                         }
-                        Deprecated::Uncategorized { note } => {
+                        Deprecated::Unspecified { note } | Deprecated::Uncategorized { note } => {
                             changes.add_change(
                                 SchemaItemType::RegistryAttributes,
                                 SchemaItemChange::Uncategorized {
@@ -410,7 +410,7 @@ impl ResolvedTelemetrySchema {
                                 },
                             );
                         }
-                        Deprecated::Uncategorized { note } => {
+                        Deprecated::Unspecified { note } | Deprecated::Uncategorized { note } => {
                             changes.add_change(
                                 schema_item_type,
                                 SchemaItemChange::Uncategorized {
