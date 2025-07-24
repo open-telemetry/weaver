71e1ca56ddf8c121550f905f03d42153509e0058
James Thompson <thompson.tomo@outlook.com>
2025-07-24 12:40:49 +1000

    Add in aggregation method

--- crates/weaver_semconv/src/aggregation.rs
+++ crates/weaver_semconv/src/aggregation.rs
@@ -11,9 +11,12 @@ use crate::YamlValue;
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
 #[serde(deny_unknown_fields)]
 pub struct AggregationSpec {
+    /// The method of aggregation to be used
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub method: Option<AggregationMethodSpec>,
     /// The parameters used in the aggregation
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub parameters: Option<HashMap<String, YamlValue>>,    
+    pub parameters: Option<HashMap<String, YamlValue>>,
 }
 
 impl AggregationSpec {
@@ -22,4 +25,22 @@ impl AggregationSpec {
     pub fn parameters(&self) -> &Option<HashMap<String, YamlValue>> {
         &self.parameters
     }
+}
+
+/// The Aggregation Method
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum AggregationMethodSpec {
+    /// Use the Default Aggregation Method.
+    Default,
+    /// Use the Sum Aggregation Method.
+    Sum,
+    /// Use the Last Value Aggregation Method.
+    LastValue,
+    /// Use the Histogram Aggregation Method.
+    Histogram,
+    /// Use the Explicit Histogram Aggregation Method.
+    ExplicitHistogram,
+    /// Use the Exponential Histogram Aggregation Method.
+    ExponentialHistogram,
 }
