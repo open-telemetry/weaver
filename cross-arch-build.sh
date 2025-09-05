c2301fd2efe4316379c85983851bcd076447ba24
alex boten <223565+codeboten@users.noreply.github.com>
2025-09-05 10:40:26 -0700

    chore: add cross arch build identifier for darwin/arm64
    Add `darwin/arm64` in the list of recognized TARGETPLATFORM to make
    getting started a little bit easier on MacOS.
    
    Signed-off-by: alex boten <223565+codeboten@users.noreply.github.com>

--- cross-arch-build.sh
+++ cross-arch-build.sh
@@ -10,6 +10,8 @@ elif [ "${TARGETPLATFORM}" = "linux/arm64" ]; then
   if [ "${TARGETPLATFORM}" != "${BUILDPLATFORM}" ]; then
     apt-get update && apt-get install -y gcc-aarch64-linux-gnu
   fi
+elif [ "${TARGETPLATFORM}" = "darwin/arm64" ]; then
+  RUST_TARGET=aarch64-apple-darwin
 else
   echo "Unsupported target platform: ${TARGETPLATFORM}"
   exit 1
