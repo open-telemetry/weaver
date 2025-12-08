18eafc855b686e416797022ebdcd84ec8f6c7008
Josh Suereth <joshuasuereth@google.com>
2025-12-08 08:38:34 -0500

    Fix merge conflict.

--- crates/weaver_resolved_schema/src/v2/mod.rs
+++ crates/weaver_resolved_schema/src/v2/mod.rs
@@ -6,7 +6,6 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use weaver_semconv::{
     deprecated::Deprecated,
-    deprecated::Deprecated,
     group::GroupType,
     v2::{
         attribute_group::AttributeGroupVisibilitySpec, signal_id::SignalId, span::SpanName,
@@ -14,12 +13,10 @@ use weaver_semconv::{
     },
 };
 use weaver_version::v2::{RegistryChanges, SchemaChanges, SchemaItemChange};
-use weaver_version::v2::{RegistryChanges, SchemaChanges, SchemaItemChange};
 
 use crate::v2::{
     attribute::Attribute,
     attribute_group::AttributeGroup,
-    catalog::{AttributeCatalog, Catalog},
     catalog::{AttributeCatalog, Catalog},
     entity::Entity,
     metric::Metric,
@@ -604,7 +601,6 @@ mod tests {
     use crate::{attribute::Attribute, lineage::GroupLineage, registry::Group};
     use weaver_semconv::{provenance::Provenance, stability::Stability};
 
-    use crate::lineage::AttributeLineage;
     use crate::lineage::AttributeLineage;
 
     use super::*;
