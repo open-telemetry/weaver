a79f52b3016cc31b2b8c77cb620da66c2418f0ed
Josh Suereth <Joshua.Suereth@gmail.com>
2026-02-01 13:09:42 -0500

    Force npm + node versions for UI
    This Fixes a few things in the build.
    
    - First it forces a specific version for npm + node - the LTS version
    - Second, it allows cargo to leverage docker in the event your local NPM fails (this may need to be configured better)
    - Finally, this provides a better caching mechanism for cargo so we're not constantly farming out to `npm`.  This is pretty critical for the docker fallback.

--- build.rs
+++ build.rs
@@ -2,7 +2,11 @@
 
 //! A build script to generate the gRPC OTLP receiver API (client and server stubs.
 
-use std::process::Command;
+use std::{
+    ffi::OsStr,
+    path::Path,
+    process::{Command, ExitStatus}, time::SystemTime,
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // The gRPC OTLP Receiver is vendored in `src/otlp_receiver/receiver` to avoid
@@ -31,45 +35,83 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+/// Helper function to determine if NPM project is out of date.
+fn is_ui_stale(dir: &Path) -> Result<bool, Box<dyn std::error::Error>> {
+    // If any output directories don't exist, rebuild.
+    if !dir.join("dist").exists() {
+        return Ok(true)
+    }
+    if !dir.join("node_modules").exists() {
+        return Ok(true)
+    }
+    // If our lock file is out of date with our package file, we need to rebuild.
+    let lock_timestamp = dir.join("package-lock.json").metadata()?.modified()?;
+    let package_timestamp = dir.join("package.json").metadata()?.modified()?;
+    if package_timestamp > lock_timestamp {
+        return Ok(true)
+    }
+    // Now check source files. This may be a bit expensive, as we continuously check last modified.
+    let last_build = dir.join("dist/index.html").metadata()?.modified()?;
+    let mut last_source_time: Option<SystemTime> = None;
+    for entry in walkdir::WalkDir::new(dir.join("src")) {
+        if let Ok(entry) = entry {
+            if let Ok(metadata) = entry.metadata() {
+                // Get the modification time for the current file/directory
+                if let Ok(modified_time) = metadata.modified() {
+                    // Update latest_time if the current file is newer
+                    if last_source_time.is_none() || modified_time > last_source_time.unwrap() {
+                        last_source_time = Some(modified_time);
+                    }
+                }
+            }
+        }
+    }
+    if let Some(time) = last_source_time {
+        return Ok(time > last_build);
+    }
+    Ok(false)
+}
+
 fn build_ui() -> Result<(), Box<dyn std::error::Error>> {
-    let ui_dir = std::path::Path::new("ui");
+    let ui_dir = Path::new("ui");
 
-    // Get the npm command - on Windows it's npm.cmd, on Unix it's npm
-    let npm_cmd = if cfg!(target_os = "windows") {
-        "npm.cmd"
-    } else {
-        "npm"
-    };
-
-    // Check if npm is available
-    let npm_check = Command::new(npm_cmd).arg("--version").output();
-
-    if npm_check.is_err() {
-        return Err(
-            "npm not found. Please install Node.js and npm from https://nodejs.org/ to build this project."
-                .into(),
-        );
+    // Check if UI is out of date before running.
+    if !is_ui_stale(ui_dir)? {
+        return Ok(())
     }
 
+    // Get the npm command - on Windows it's npm.cmd, on Unix it's npm
+    let mut npm_runner = if cfg!(target_os = "windows") {
+        NpmRunner::NpmExec("npm.cmd".to_owned())
+    } else {
+        NpmRunner::NpmExec("npm".to_owned())
+    };
+
+    if !npm_runner.check_valid() {
+        println!("cargo:warning=npm not found. Please install Node.js and npm from https://nodejs.org/ to build this project.");
+        println!("cargo:warning=Attempting to use docker for now.");
+        // TODO - Docker usage should ALWAYS be behind some kind of flag, ideally.
+        npm_runner = NpmRunner::Docker;
+    }
+
+    // Check if npm is available
     println!("cargo:warning=Building UI...");
 
     // Always update dependencies to exactly match latest package-lock.json
     println!("cargo:warning=Checking UI dependencies...");
-    let status = Command::new(npm_cmd)
-        .arg("ci")
-        .current_dir(ui_dir)
-        .status()?;
+    let status = npm_runner.run(ui_dir, vec!["ci"])?;
 
     if !status.success() {
-        return Err("Failed to install UI dependencies".into());
+        println!("cargo:warning=Unable to use installed npm, using docker instead...");
+        npm_runner = NpmRunner::Docker;
+        let status = npm_runner.run(ui_dir, vec!["ci"])?;
+        if !status.success() {
+            return Err("Failed to load UI dependencies".into());
+        }
     }
 
     // Build the UI
-    let status = Command::new(npm_cmd)
-        .arg("run")
-        .arg("build")
-        .current_dir(ui_dir)
-        .status()?;
+    let status = npm_runner.run(ui_dir, vec!["run", "build"])?;
 
     if !status.success() {
         return Err("Failed to build UI".into());
@@ -78,4 +120,46 @@ fn build_ui() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:warning=UI build complete");
 
     Ok(())
+}
+
+enum NpmRunner {
+    NpmExec(String),
+    Docker,
+}
+
+impl NpmRunner {
+    // Retruns true if this is a valid way to run NPM.
+    fn check_valid(&self) -> bool {
+        match self {
+            NpmRunner::NpmExec(npm_cmd) => Command::new(npm_cmd).arg("--version").output().is_ok(),
+            // TODO - figure out how to test docker install.
+            NpmRunner::Docker => true,
+        }
+    }
+
+    /// Runs the given NPM command given a chosen runner.
+    fn run<I, S>(&self, dir: &Path, cmd: I) -> Result<ExitStatus, Box<dyn std::error::Error>>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        let result = match self {
+            NpmRunner::NpmExec(npm) => Command::new(npm).args(cmd).current_dir(dir).status()?,
+            NpmRunner::Docker => {
+                Command::new("docker")
+                .arg("run")
+                .arg("--rm")
+                .arg("-v")
+                .arg(".:/app")
+                .arg("-w")
+                .arg("/app")
+                // TODO - This version should get pulled from somewhere.
+                .arg("node:lts-alpine")
+                .arg("npm")
+                .args(cmd)
+                .current_dir(dir)
+                .status()?},
+        };
+        Ok(result)
+    }
 }
