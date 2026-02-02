b68f6c3299f878dc8e696ea374702712de7cff54
jerbly <jeremyblythe@gmail.com>
2026-02-01 22:50:52 -0500

    Dockerfile musl dependencies, checksums for wget in script

--- cross-arch-build.sh
+++ cross-arch-build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -exu
 
-# Install build dependencies
-apt-get update && apt-get install -y musl-tools musl-dev perl
-
 # Detect actual host architecture (not Docker platform vars which may differ with QEMU)
 HOST_ARCH=$(uname -m)
 echo "Host architecture: ${HOST_ARCH}"
@@ -19,8 +16,8 @@ case "${TARGETPLATFORM}" in
       export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER=musl-gcc
     else
       # Cross-compiling to x86_64 from another arch
-      apt-get install -y wget
       wget -q https://musl.cc/x86_64-linux-musl-cross.tgz
+      echo "52abd1a56e670952116e35d1a62e048a9b6160471d988e16fa0e1611923dd108a581d2e00874af5eb04e4968b1ba32e0eb449a1f15c3e4d5240ebe09caf5a9f3  x86_64-linux-musl-cross.tgz" | sha512sum -c -
       tar xf x86_64-linux-musl-cross.tgz -C /opt
       export PATH="/opt/x86_64-linux-musl-cross/bin:$PATH"
       export CC_x86_64_unknown_linux_musl=x86_64-linux-musl-gcc
@@ -35,8 +32,8 @@ case "${TARGETPLATFORM}" in
       export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER=musl-gcc
     else
       # Cross-compiling to arm64 from x86_64
-      apt-get install -y wget
       wget -q https://musl.cc/aarch64-linux-musl-cross.tgz
+      echo "8695ff86979cdf30fbbcd33061711f5b1ebc3c48a87822b9ca56cde6d3a22abd4dab30fdcd1789ac27c6febbaeb9e5bde59d79d66552fae53d54cc1377a19272  aarch64-linux-musl-cross.tgz" | sha512sum -c -
       tar xf aarch64-linux-musl-cross.tgz -C /opt
       export PATH="/opt/aarch64-linux-musl-cross/bin:$PATH"
       export CC_aarch64_unknown_linux_musl=aarch64-linux-musl-gcc
