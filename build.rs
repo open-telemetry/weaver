aee5e15f0af65ad0f1bf1c83a1632682e5e690b4
Josh Suereth <joshuasuereth@google.com>
2026-02-12 13:17:02 -0500

    Move to pnpm instead of npm. Update cargo build to NOT force build the UI but request you do it locally.

--- build.rs
+++ build.rs
@@ -3,9 +3,7 @@
 //! A build script to generate the gRPC OTLP receiver API (client and server stubs.
 
 use std::{
-    ffi::OsStr,
     path::Path,
-    process::{Command, ExitStatus},
     time::SystemTime,
 };
 
@@ -31,7 +29,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //     )?;
 
     // Build the UI
-    build_ui()?;
+    check_ui()?;
 
     Ok(())
 }
@@ -39,6 +37,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn timestamp(dir: walkdir::DirEntry) -> Result<SystemTime, Box<dyn std::error::Error>> {
     let md = dir.metadata()?;
     Ok(md.modified()?)
+}
+
+/// Helper function to determine if package-lock file is out of date.
+fn is_package_lock_stale(dir: &Path) -> Result<bool, Box<dyn std::error::Error>> {
+    let lock_timestamp = dir.join("pnpm-lock.yaml").metadata()?.modified()?;
+    let package_timestamp = dir.join("package.json").metadata()?.modified()?;
+    if package_timestamp > lock_timestamp {
+        return Ok(true);
+    }
+    return Ok(false);
 }
 
 /// Helper function to determine if NPM project is out of date.
@@ -50,14 +58,12 @@ fn is_ui_stale(dir: &Path) -> Result<bool, Box<dyn std::error::Error>> {
     if !dir.join("node_modules").exists() {
         return Ok(true);
     }
-    // If our lock file is out of date with our package file, we need to rebuild.
-    let lock_timestamp = dir.join("package-lock.json").metadata()?.modified()?;
-    let package_timestamp = dir.join("package.json").metadata()?.modified()?;
-    if package_timestamp > lock_timestamp {
-        return Ok(true);
-    }
     // Now check source files. This may be a bit expensive, as we continuously check last modified.
     let last_build = dir.join("dist/index.html").metadata()?.modified()?;
+    let lock_timestamp = dir.join("pnpm-lock.yaml").metadata()?.modified()?;
+    if lock_timestamp > last_build {
+        return Ok(true);
+    }
     for entry in walkdir::WalkDir::new(dir.join("src")) {
         if timestamp(entry?)? > last_build {
             return Ok(true);
@@ -66,95 +72,19 @@ fn is_ui_stale(dir: &Path) -> Result<bool, Box<dyn std::error::Error>> {
     Ok(false)
 }
 
-fn build_ui() -> Result<(), Box<dyn std::error::Error>> {
+fn check_ui() -> Result<(), Box<dyn std::error::Error>> {
     let ui_dir = Path::new("ui");
 
     // Check if UI is out of date before running.
-    if !is_ui_stale(ui_dir)? {
-        return Ok(());
+    if is_ui_stale(ui_dir)? {
+        return Err("Weaver UI is out of date. Please run `pnpm build` in the `ui` directory.".into());
     }
 
-    // Get the npm command - on Windows it's npm.cmd, on Unix it's npm
-    let mut npm_runner = if cfg!(target_os = "windows") {
-        NpmRunner::NpmExec("npm.cmd".to_owned())
-    } else {
-        NpmRunner::NpmExec("npm".to_owned())
-    };
-
-    if !npm_runner.check_valid() {
-        println!("cargo:warning=npm not found. Please install Node.js and npm from https://nodejs.org/ to build this project.");
-        println!("cargo:warning=Attempting to use docker for now.");
-        // TODO - Docker usage should ALWAYS be behind some kind of flag, ideally.
-        npm_runner = NpmRunner::Docker;
+    // Check if we need to install packages.
+    if is_package_lock_stale(ui_dir)? {
+        // TODO - Disable for CI
+        return Err("Weaver `ui/pnpm-lock.yaml` is out of date. Please run `pnpm install` in the `ui` directory.".into());
     }
-
-    // Check if npm is available
-    println!("cargo:warning=Building UI...");
-
-    // Always update dependencies to exactly match latest package-lock.json
-    println!("cargo:warning=Checking UI dependencies...");
-    let status = npm_runner.run(ui_dir, vec!["ci"])?;
-
-    if !status.success() {
-        println!("cargo:warning=Unable to use installed npm, using docker instead...");
-        npm_runner = NpmRunner::Docker;
-        let status = npm_runner.run(ui_dir, vec!["ci"])?;
-        if !status.success() {
-            return Err("Failed to load UI dependencies".into());
-        }
-    }
-
-    // Build the UI
-    let status = npm_runner.run(ui_dir, vec!["run", "build"])?;
-
-    if !status.success() {
-        return Err("Failed to build UI".into());
-    }
-
-    println!("cargo:warning=UI build complete");
 
     Ok(())
-}
-
-enum NpmRunner {
-    NpmExec(String),
-    Docker,
-}
-
-impl NpmRunner {
-    // Returns true if this is a valid way to run NPM.
-    fn check_valid(&self) -> bool {
-        match self {
-            NpmRunner::NpmExec(npm_cmd) => Command::new(npm_cmd).arg("--version").output().is_ok(),
-            // TODO - figure out how to test docker install.
-            NpmRunner::Docker => true,
-        }
-    }
-
-    /// Runs the given NPM command given a chosen runner.
-    fn run<I, S>(&self, dir: &Path, cmd: I) -> Result<ExitStatus, Box<dyn std::error::Error>>
-    where
-        I: IntoIterator<Item = S>,
-        S: AsRef<OsStr>,
-    {
-        let result = match self {
-            NpmRunner::NpmExec(npm) => Command::new(npm).args(cmd).current_dir(dir).status()?,
-            NpmRunner::Docker => {
-                Command::new("docker")
-                    .arg("run")
-                    .arg("--rm")
-                    .arg("-v")
-                    .arg(".:/app")
-                    .arg("-w")
-                    .arg("/app")
-                    // TODO - This version should get pulled from somewhere.
-                    .arg("node:lts-alpine")
-                    .arg("npm")
-                    .args(cmd)
-                    .current_dir(dir)
-                    .status()?
-            }
-        };
-        Ok(result)
-    }
 }
