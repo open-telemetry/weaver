e1b672c30e583d99ba08af821943c23bbec67ad4
jerbly <jeremyblythe@gmail.com>
2026-05-24 21:13:03 -0400

    follow best practice

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -2,15 +2,9 @@
 
 cd "$SRC/weaver"
 
-cargo +nightly fuzz build
+cargo +nightly fuzz build -O --debug-assertions
 
 FUZZ_OUT="fuzz/target/x86_64-unknown-linux-gnu/release"
-cp "$FUZZ_OUT/live_check_json"       "$OUT/"
-cp "$FUZZ_OUT/live_check_text"       "$OUT/"
-cp "$FUZZ_OUT/semconv_yaml"          "$OUT/"
-cp "$FUZZ_OUT/semconv_manifest_yaml" "$OUT/"
-cp "$FUZZ_OUT/forge_config_yaml"     "$OUT/"
-cp "$FUZZ_OUT/weaver_config_toml"    "$OUT/"
-cp "$FUZZ_OUT/policy_rego"           "$OUT/"
-cp "$FUZZ_OUT/forge_jq"              "$OUT/"
-cp "$FUZZ_OUT/forge_jinja"           "$OUT/"
+for f in fuzz/fuzz_targets/*.rs; do
+    cp "$FUZZ_OUT/$(basename "${f%.*}")" "$OUT/"
+done
